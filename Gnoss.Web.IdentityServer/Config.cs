5a2f1dbc11833d1a2ae295cbac37bcaed121a6c9
davidruiz-gnoss <davidruiz@gnoss.com>
2022-09-09 12:03:34 +0200

    Security Improvement

---
+++ Gnoss.Web.IdentityServer/Config.cs
@@ -0,0 +1,71 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gnoss.Web.IdentityServer
+{
+    public class Config
+    {
+        /// <summary>
+        /// Obtiene los clientes configurados
+        /// </summary>
+        public static IEnumerable<Client> GetClients(int pTiempo, string pClienteID, string pClienteSecret, string pScope)
+        {
+            return new List<Client>
+            {
+                // ClientId es el parámetro del servicio que solicita acceso a través de 'client_id'
+                // ClientSecrets es el parámetro del servicio que solicita acceso a través de 'client_secret'
+                // AllowedScopes son los scopes a los que se le da acceso al servicio
+                
+                // Cliente para el servicio externo (en desuso)
+                new Client
+                {
+                    ClientId = pClienteID,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(pClienteSecret.Sha256())
+                    },
+                    AllowedScopes = 
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        pScope
+                        
+                    },
+                    AccessTokenLifetime = pTiempo,
+                    AccessTokenType = AccessTokenType.Jwt
+                },
+            };
+        }
+
+        // APIs allowed to access the Auth server
+        public static IEnumerable<ApiResource> GetApiResources(string pScope)
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource()
+                {
+                    Name = pScope,   //This is the name of the API
+                    Description = "This is the invoice Api-resource description",
+                    Enabled = true,
+                    DisplayName = "Identity Server API",
+                    Scopes = new List<string> { pScope }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes(string pScope)
+        {
+            return new[]
+            {
+                new ApiScope(pScope, "Identity Server API"),
+            };
+        }
+
+    }
+}
---
+++ Gnoss.Web.IdentityServer/Startup.cs
@@ -0,0 +1,161 @@
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Gnoss.Web.IdentityServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+
+            string issuerUri = "";
+            if (environmentVariables.Contains("IssuerUri"))
+            {
+                issuerUri = environmentVariables["IssuerUri"] as string;
+            }
+            else
+            {
+                issuerUri = Configuration.GetConnectionString("IssuerUri");
+            }
+
+            int tiempo = 0;
+            try
+            {
+                if (environmentVariables.Contains("segundostoken"))
+                {
+                    tiempo = int.Parse(environmentVariables["segundostoken"] as string);
+                }
+                else
+                {
+                    tiempo = int.Parse(Configuration.GetConnectionString("segundostoken"));
+                }
+            }
+            catch { }
+            if (tiempo == 0)
+            {
+                tiempo = 86400;
+            }
+            string scopeIdentity = "";
+            if (environmentVariables.Contains("scopeIdentity"))
+            {
+                scopeIdentity = environmentVariables["scopeIdentity"] as string;
+            }
+            else
+            {
+                scopeIdentity = Configuration.GetConnectionString("scopeIdentity");
+            }
+
+            string clientIDIdentity = "";
+            if (environmentVariables.Contains("clientIDIdentity"))
+            {
+                clientIDIdentity = environmentVariables["clientIDIdentity"] as string;
+            }
+            else
+            {
+                clientIDIdentity = Configuration.GetConnectionString("clientIDIdentity");
+            }
+
+            string clientIDSecret = "";
+            if (environmentVariables.Contains("clientSecretIdentity"))
+            {
+                clientIDSecret = environmentVariables["clientSecretIdentity"] as string;
+            }
+            else
+            {
+                clientIDSecret = Configuration.GetConnectionString("clientSecretIdentity");
+                
+            }
+
+
+            // Add Cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "_myAllowSpecificOrigins",
+                builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                });
+            });
+            services.AddIdentityServer(x =>
+            {
+                x.IssuerUri = issuerUri;
+            }).AddDeveloperSigningCredential()
+               .AddInMemoryPersistedGrants()
+               .AddInMemoryCaching()
+               .AddInMemoryClients(Clientes(tiempo, clientIDIdentity, clientIDSecret, scopeIdentity))
+               .AddInMemoryApiResources(ApiResources(scopeIdentity))
+               .AddInMemoryApiScopes(apiScopes(scopeIdentity));
+            
+
+
+            services.AddControllers();
+            
+
+        }
+
+        private IEnumerable<ApiScope> apiScopes(string pScope)
+        {
+            return Config.GetApiScopes(pScope);
+        }
+
+        private IEnumerable<ApiResource> ApiResources(string pScope)
+        {
+            return Config.GetApiResources(pScope);
+        }
+
+        private IEnumerable<Client> Clientes(int pTiempo, string pClienteID, string pClienteSecret, string pScope)
+        {
+            return Config.GetClients(pTiempo, pClienteID, pClienteSecret, pScope);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseCors();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
