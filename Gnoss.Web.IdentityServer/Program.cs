031869562add0f510b3200b368c526f5bb45db40
Javier-RSP <javierruiz@gnoss.com>
2026-04-13 11:59:56 +0200

    Security Improvement

--- Gnoss.Web.IdentityServer/Program.cs
+++ Gnoss.Web.IdentityServer/Program.cs
@@ -1,11 +1,9 @@
+using Es.Riam.Gnoss.Util.General;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Gnoss.Web.IdentityServer
 {
@@ -13,7 +11,19 @@ namespace Gnoss.Web.IdentityServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            LoggingService.ConfigurarBasicStartupSerilog().CreateBootstrapLogger();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error fatal durante el arranque");
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // asegura que se escriben todos los logs pendientes
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -23,6 +33,7 @@ namespace Gnoss.Web.IdentityServer
                     config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
                     config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
                 })
+                .UseSerilog((context, services, configuration) => LoggingService.ConfigurarSerilog(context.Configuration, services, configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
